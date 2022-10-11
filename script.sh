d563b135444c53bf8264fbce4557b653071a642a
Igor <igor.majerczak@student.uj.edu.pl>
2022-10-11 21:13:48 +0200

    stage 2 - performing basic calculations

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ possibleOperations='^[-,+,*,/]$'
 echo "Enter an arithmetic operation:"
 read number1 operation number2
 if [[ "$number1" =~ $numberRegex ]] && [[ "$operation" =~ $possibleOperations ]] && [[ "$number2" =~ $numberRegex ]]; then
-    echo "Operation check passed!"
+    echo $(( "$number1 $operation $number2"))
 else
     echo "Operation check failed!"
 fi
