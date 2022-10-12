5f23f0991ee2fa9d61341de89abf9c5c9a85fbb3
Igor <igor.majerczak@student.uj.edu.pl>
2022-10-12 11:50:44 +0200

    stage 4/4 - added main loop and saving to a file

--- script.sh
+++ script.sh
@@ -2,10 +2,19 @@
 numberRegex='^-?[0-9]+.?[0-9]*$'
 possibleOperations='^[-,+,*,/,^]$'
 
-echo "Enter an arithmetic operation:"
-read number1 operation number2
-if [[ "$number1" =~ $numberRegex ]] && [[ "$operation" =~ $possibleOperations ]] && [[ "$number2" =~ $numberRegex ]]; then
-    echo $( bc -l <<< "scale=2; $number1 $operation $number2" ) 
-else
-    echo "Operation check failed!"
-fi
+offSwitch=1
+echo "Welcome to the basic calculator!" | tee -a operation_history.txt
+
+while [ $offSwitch -eq 1 ]; do
+	echo "Enter an arithmetic operation or type 'quit' to quit:"
+	read number1 operation number2
+    echo "$number1 $operation $number2" >> operation_history.txt
+	if [ "$number1" == "quit" ]; then
+        echo "Goodbye!"
+		offSwitch=0
+	elif [[ "$number1" =~ $numberRegex ]] && [[ "$operation" =~ $possibleOperations ]] && [[ "$number2" =~ $numberRegex ]]; then
+    		echo $( bc -l <<< "scale=2; $number1 $operation $number2" ) 
+	else
+    		echo "Operation check failed!"
+	fi
+done | tee -a operation_history.txt
