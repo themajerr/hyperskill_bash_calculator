eb5850a7669363ae8fd1ea5936a05caba4c48ea8
Igor <igor.majerczak@student.uj.edu.pl>
2022-10-12 10:40:54 +0200

    stage 3 - math done with bc command + additional operation (exponentiation) added

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-numberRegex='^-?[0-9]+$'
-possibleOperations='^[-,+,*,/]$'
+numberRegex='^-?[0-9]+.?[0-9]*$'
+possibleOperations='^[-,+,*,/,^]$'
 
 echo "Enter an arithmetic operation:"
 read number1 operation number2
 if [[ "$number1" =~ $numberRegex ]] && [[ "$operation" =~ $possibleOperations ]] && [[ "$number2" =~ $numberRegex ]]; then
-    echo $(( "$number1 $operation $number2"))
+    echo $( bc -l <<< "scale=2; $number1 $operation $number2" ) 
 else
     echo "Operation check failed!"
 fi
